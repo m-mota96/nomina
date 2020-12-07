7f95d1f7d68fa7901c501e90d6f58e02153d46b0
vagrant <vagrant@homestead>
2020-12-07 21:35:51 +0000

    v 2

---
+++ database/migrations/2020_12_07_212949_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 255);
+            $table->string('name', 255);
+            $table->string('last_name', 255);
+            $table->string('first_name', 255);
+            $table->string('email', 255);
+            $table->string('contract_type');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
